6a4b33ac1a5c25734110b5fe34892a9f731897cf
chenqin <490358423@qq.com>
2025-04-09 18:11:32 +0800

    don't use hashmap, use syntactic sugar for the input

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,29 +1,21 @@
-use std::collections::HashMap;
-
-use log_table::logger::Logger;
+use log_table::{logger::Logger, table_data};
 
 fn main() {
     let logger = Logger::new();
+    
+    let chain_id = "1";
+    let from_token = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+    let to_token = "0x420000000000000000000000000000000000006";
+    let from_amount = "1500000000000000000"; // 1.5 ETH in wei
+    let from_address = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+    let options = r#"{"slippage": "0.5%", "deadline": 3600}"#;
 
-    let mut data = HashMap::new();
-    data.insert("chainId", "1".to_string());
-    data.insert(
-        "fromToken",
-        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266".to_string(),
-    );
-    data.insert(
-        "toToken",
-        "0x420000000000000000000000000000000000006".to_string(),
-    );
-    data.insert("amount", "1.5 ether".to_string());
-    data.insert(
-        "fromAddress",
-        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266".to_string(),
-    );
-    data.insert(
-        "options",
-        r#"{"slippage": "0.5%", "deadline": 3600}"#.to_string(),
-    );
-
-    logger.table("Processing Missed Event", data);
+    logger.table("LiFi Routes", table_data! {
+        "chainId" => chain_id,
+        "fromToken" => from_token,
+        "toToken" => to_token,
+        "amount" => format!("{} ether", from_amount),
+        "fromAddress" => from_address,
+        "options" => options,
+    });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
 pub mod logger;
+
+#[macro_export]
+macro_rules! table_data {
+    ($($key:expr => $value:expr),* $(,)?) => {{
+        let mut map = ::std::collections::HashMap::new();
+        $(
+            map.insert($key, $value.to_string());
+        )*
+        map
+    }};
+}
