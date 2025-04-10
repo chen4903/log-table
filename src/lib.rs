c68382a7f2496abdbf14aa25065838a4a791e0ac
chenqin <490358423@qq.com>
2025-04-10 16:55:17 +0800

    fix macro error

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 pub mod color;
 pub mod logger;
 
+// Re-export indexmap for macro use
+pub use indexmap;
+
 #[macro_export]
 macro_rules! table_data {
     ($($key:ident : $value:expr),* $(,)?) => {{
-        let mut map = ::indexmap::IndexMap::new();
+        let mut map = $crate::indexmap::IndexMap::new();
         $(
             let value_str = format!("{}", $value);
             map.insert(stringify!($key), value_str);
